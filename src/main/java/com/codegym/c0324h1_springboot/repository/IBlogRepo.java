cd45062ff00152cb59eae8190d30a63eb6091729
duongnhat0801 <nhatx0801@hmail.com>
2024-07-23 08:50:58 +0700

    update lan 4

--- src/main/java/com/codegym/c0324h1_springboot/repository/IBlogRepo.java
+++ src/main/java/com/codegym/c0324h1_springboot/repository/IBlogRepo.java
@@ -1,12 +1,13 @@
 package com.codegym.c0324h1_springboot.repository;
 
 import com.codegym.c0324h1_springboot.model.Blog;
-import jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle;
+
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface IBlogRepo extends JpaRepository<Blog, Long> {
     @Query(nativeQuery = true, value = "select * from blog as s where s.title like :title")
-    Page<Blog> findAllTitle(HtmlStyle htmlStyle);
+    Page<Blog> findAllTitle(String title, Pageable pageable);
 }
